80e733213fd4dedb6d18e08cd85cb6fdeec0101f
nikiten <nikitenuss@mail.ru>
2026-08-16 16:57:55 +0300

    feat: render toast container for global errors

--- src/app/providers/AppProviders.tsx
+++ src/app/providers/AppProviders.tsx
@@ -1,6 +1,9 @@
 'use client'
 
+import 'react-toastify/dist/ReactToastify.css'
+
 import type { ReactNode } from 'react'
+import { ToastContainer } from 'react-toastify'
 
 import { QueryProvider } from './QueryProvider'
 import { SessionBootstrap } from './SessionBootstrap'
@@ -12,5 +15,12 @@ type Props = {
 export const AppProviders = ({ children }: Props) => (
   <QueryProvider>
     <SessionBootstrap>{children}</SessionBootstrap>
+    <ToastContainer
+      position="bottom-left"
+      autoClose={5000}
+      hideProgressBar
+      closeButton={false}
+      icon={false}
+    />
   </QueryProvider>
 )
